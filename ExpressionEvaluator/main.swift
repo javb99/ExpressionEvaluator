d01ea196b9928100b900c86cd1a918de2aba4b6a
Joseph Van Boxtel <dev.josephvanboxtel@gmail.com>
2020-04-16 17:01:39 -0700

    Initial Commit

---
+++ ExpressionEvaluator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExpressionEvaluator
+//
+//  Created by Joseph Van Boxtel on 4/16/20.
+//  Copyright © 2020 Joseph Van Boxtel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
